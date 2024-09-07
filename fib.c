81c77762f5331fcd6a9a1d1e1f96ea533579329a
student <student@tuffix-vm.myguest.virtualbox.org>
2024-09-06 21:46:09 -0700

    Unfinished

--- fib.c
+++ fib.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-   printf("Hello!\n");
+int get_fib(int n)
+{
+    if (n == 0) {return 0;}
+    if (n == 1) {return 1;}
+    return get_fib(n - 1) + get_fib(n - 2);
+}
+
+int main(int argc, char *argv[]){
+   int result = get_fib(atoi(argv[1]) + 2);
+   printf("%d\n", result);
    return 0;
 }
