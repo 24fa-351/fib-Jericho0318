cc291109442bdd0af39ea664f1d826347d0c2bd3
student <student@tuffix-vm.myguest.virtualbox.org>
2024-09-08 18:41:28 -0700

    Finished

--- fib.c
+++ fib.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-unsigned int get_fib(unsigned int n)
+unsigned long long int recur_fib(unsigned long long int n)
 {
     if (n == 0) {return 0;}
     if (n == 1) {return 1;}
-    return get_fib(n - 1) + get_fib(n - 2);
+    return recur_fib(n - 1) + recur_fib(n - 2);
+}
+
+unsigned long long int iter_fib(unsigned long long int n)
+{
+    unsigned long long int first = 0, second = 1, sum = 0;
+    if (n == 0) {return 0;}
+    if (n == 1) {return 1;}
+    for (int i = 2; i <= n; ++i)
+    {
+    	sum    = first + second;
+    	first  = second;
+    	second = sum;
+    }
+    return sum;
 }
 
 int main(int argc, char *argv[]){
@@ -19,14 +34,16 @@ int main(int argc, char *argv[]){
    		printf("File not found\n");
    		return 0;
    	}
-   fprintf(file, "hello");
-   fclose(file);
    }
    char line[atoi(argv[1])];
-   unsigned int num;
-   if (fscanf(file, "%u", &num) == 1)
+   unsigned long long int get_num = atoi(fgets(line, sizeof(line), file));
+   if (!strcmp(argv[2], "i"))
    {
-   	printf("%u\n", get_fib(atoi(argv[1]) + 2));
+   	printf("%llu\n", iter_fib(get_num + 3));
+   }
+   else if (!strcmp(argv[2], "r"))
+   {
+   	printf("%llu\n", recur_fib(get_num + 4));
    }
    fclose(file);
    return 0;
