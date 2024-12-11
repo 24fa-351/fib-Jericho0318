e0a1fc43700f25e7abe645b508129682ba17081e
Jericho0318 <111917174+Jericho0318@users.noreply.github.com>
2024-12-10 20:31:51 -0800

    Formatted

--- fib.c
+++ fib.c
@@ -1,32 +1,34 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
-uint64_t recur_fib(uint64_t num)
-{
-    if (num == 0) {return 0;}
-    if (num == 1) {return 1;}
-    return recur_fib(num - 1) + recur_fib(num - 2);
+uint64_t recur_fib(uint64_t n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return recur_fib(n - 1) + recur_fib(n - 2);
 }
 
-uint64_t iter_fib(uint64_t num)
-{
+uint64_t iter_fib(uint64_t n) {
     uint64_t first = 0, second = 1, sum = 0;
-    if (num == 0) {return 0;}
-    if (num == 1) {return 1;}
-    for (int ix = 0; ix < num; ++ix)
-    {
-    	sum    = first + second;
-    	first  = second;
-    	second = sum;
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    for (int i = 0; i < n; ++i) {
+        sum = first + second;
+        first = second;
+        second = sum;
     }
     return sum;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     if (argc != 4) {
-        perror("Provide 4 arguments: ./<filename> <num> <i or r> <filename.txt>\n");
+        perror(
+            "Provide 4 arguments: ./<filename> <num> <i/r> <filename.txt>\n");
         exit(1);
     }
 
@@ -38,15 +40,16 @@ int main(int argc, char *argv[]){
     }
 
     uint64_t file_num;
+    char line[1024];
     uint64_t get_num = atoi(argv[1]);
 
     fscanf(file, "%ld", &file_num);
-    uint64_t sum = get_num + file_num;
+    uint64_t sum_of_N = get_num + file_num;
 
     if (!strcmp(argv[2], "i")) {
-        printf("%lu\n", iter_fib(sum - 2));
+        printf("%lu\n", iter_fib(sum_of_N - 2));
     } else if (!strcmp(argv[2], "r")) {
-        printf("%lu\n", recur_fib(sum - 1));
+        printf("%lu\n", recur_fib(sum_of_N - 1));
     } else {
         perror("Choose between i or r in argv[2]\n");
         exit(1);
